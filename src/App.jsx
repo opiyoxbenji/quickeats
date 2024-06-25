4d693edbe19e71f378704ce582985a2056024b0d
pixelbushi <opiyosbenji@gmail.com>
2024-06-25 08:34:35 +0300

    minor updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import { useState } from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
-	const [count, setCount] = useState(0);
-
-	return (
-		<>
-			<h1>Tweeet</h1>
-		</>
-	);
+	return <h1 className=''>
+		<Navbar />
+	</h1>;
 }
 
 export default App;
