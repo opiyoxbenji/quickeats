ba69ebf3e9f3b8423eb010a1e90bd22ce5dce8dd
Jassocc <clnearle@gmail.com>
2024-06-29 09:27:27 +0200

    Base of User Controller

---
+++ Backend/controllers/userController.js
@@ -0,0 +1,14 @@
+import userModel from "../models/userModel.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import validator from "validator";
+
+const userLogin = async (req, res) => {
+
+}
+
+const userRegistration = async (req, res) => {
+
+}
+
+export {userLogin, userRegistration};
