b3afd3519dd1437a5a2c76d2bd33d7d206c719fd
Jassocc <clnearle@gmail.com>
2024-07-02 16:56:01 +0200

    merging branches

--- src/StoreContext/StoreContext.jsx
+++ src/StoreContext/StoreContext.jsx
@@ -8,6 +8,18 @@ const StoreContextProvider = (props) => {
     const foodurl = "http://localhost:4000/api/food/list"
     const [token, setToken] = useState("");
     const [food_list, setFoodLIst] = useState([]);
+    const [cartItems, setCartItems] = useState({});
+
+    const addToCart = (itemId) => {
+        if (!cartItems[itemId]) {
+            setCartItems((prev) => ({...prev, [itemId]: 1}))
+        } else {
+            setCartItems((prev) => ({...prev, [itemId]:prev[itemId] + 1}))
+        }
+    }
+    const removeFromCart = (itemId) => {
+        setCartItems((prev) => ({...prev, [itemId]:prev[itemId] - 1}))
+    }
     
     const getFood = async () => {
         const res = await axios.get(foodurl);
@@ -22,11 +34,18 @@ const StoreContextProvider = (props) => {
         }
         loadData();
     },[])
+    useEffect(() => {
+        console.log(cartItems);
+    }, [cartItems])
     const contextValue = {
         food_list,
         url,
         token,
-        setToken
+        setToken,
+        cartItems,
+        setCartItems,
+        addToCart,
+        removeFromCart
     }
     return (
         <StoreContext.Provider value={contextValue}>
