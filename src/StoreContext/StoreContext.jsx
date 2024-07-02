79487585cb231399275730c72c026f4a36ddd36d
Jassocc <clnearle@gmail.com>
2024-07-02 13:09:37 +0200

    undo commit revert back

--- src/StoreContext/StoreContext.jsx
+++ src/StoreContext/StoreContext.jsx
@@ -4,22 +4,10 @@ import axios from 'axios';
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
-    const [cartItems, setCartItems] = useState({});
     const url = "http://localhost:4000";
     const foodurl = "http://localhost:4000/api/food/list"
     const [token, setToken] = useState("");
     const [food_list, setFoodLIst] = useState([]);
-    
-    const addToCart = (itemId) => {
-        if (!cartItems[itemId]) {
-            setCartItems((prev) => ({...prev,[itemId]:1}))
-        } else {
-            setCartItems((prev) => ({...prev, [itemId]:prev[itemId]+1}))
-        }
-    }
-    const removeFromCart = (itemId) => {
-        setCartItems((prev) =>({...prev, [itemId]:prev[itemId]-1}))
-    }
     
     const getFood = async () => {
         const res = await axios.get(foodurl);
@@ -34,18 +22,11 @@ const StoreContextProvider = (props) => {
         }
         loadData();
     },[])
-    useEffect(() => {
-        console.log(cartItems);
-    }, [cartItems])
     const contextValue = {
         food_list,
         url,
         token,
-        setToken,
-        cartItems,
-        setCartItems,
-        addToCart,
-        removeFromCart
+        setToken
     }
     return (
         <StoreContext.Provider value={contextValue}>
