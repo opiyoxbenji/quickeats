06dfca7fa12f6cd57ebccdca4c20a19ba316ba16
Jassocc <clnearle@gmail.com>
2024-07-02 17:39:49 +0200

    Fixed issue where cart and menu were coliding

--- src/components/FoodsDisplay/FoodItem.jsx
+++ src/components/FoodsDisplay/FoodItem.jsx
@@ -1,24 +1,25 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import './Fooditem.css'
 import { StoreContext } from "../../StoreContext/StoreContext";
 import assets from '../../assets/assets';
 
-
 const FoodItem = ({id, name, price, description, image}) => {
-    const  [itemCount, setItemCount] = useState(0);
-    const { url } = useContext(StoreContext);
-    return(
-        <div className="food_item ">
+    const { cartItems, addToCart, removeFromCart } = useContext(StoreContext);
+    const itemCount = cartItems[id] || 0; // Get item count from cartItems
+
+    return (
+        <div className="food_item">
             <div className="food_item_image_container">
                 <img className="food_image" src={"http://localhost:4000/images/" + image} alt=""/>
-                {!itemCount
-                    ?<img className="add" onClick={() => setItemCount(prev=>prev+1)} src={assets.add_icon} alt="" />
-                    :<div className="food-item-counter" >
-                        <img onClick={() => setItemCount(prev=>prev-1)} src={assets.red_minus} alt="" />
+                {!itemCount ? (
+                    <img className="add" onClick={() => addToCart(id)} src={assets.add_icon} alt="" />
+                ) : (
+                    <div className="food-item-counter">
+                        <img onClick={() => removeFromCart(id)} src={assets.red_minus} alt="" />
                         <p>{itemCount}</p>
-                        <img onClick={() => setItemCount(prev=>prev+1)} src={assets.green_plus} alt="" />
+                        <img onClick={() => addToCart(id)} src={assets.green_plus} alt="" />
                     </div>
-                }
+                )}
             </div>
             <div className="food_item_name">
                 <p>{name}</p>
